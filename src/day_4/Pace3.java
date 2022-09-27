871d1a64c17b7b91d2b842455a32a249c8e81d76
baer1414 <bahareren1416@gmail.com>
2022-09-27 22:38:12 +0300

    second

---
+++ src/day_4/Pace3.java
@@ -0,0 +1,9 @@
+package day_4;
+
+public class Pace3 {
+    public static void main(String[] args) {
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+        System.out.println("merhaba");
+    }
+}
